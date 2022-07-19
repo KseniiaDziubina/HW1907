4db07d934be53641cbaab83cdf2e57dceb354aaf
Kseniia <ksenjadzjubina@gmail.com>
2022-07-20 00:36:15 +0300

    Задача 21

---
+++ Task21/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число   ");
+int i;
+int Number = Convert.ToInt32(Console.ReadLine());
+double Cube;
+for (i=1;  i<Number; i++)
+{
+    Cube = Math.Pow(i,3); 
+    Console.Write($"{Cube}, ");
+}
+Cube = Math.Pow(i, 3);
+Console.WriteLine(Cube);
