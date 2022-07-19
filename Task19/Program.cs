6d79b5b765f6547ea30180c214894f4f0d4278ea
Kseniia <ksenjadzjubina@gmail.com>
2022-07-20 00:32:08 +0300

    Задача 19

---
+++ Task19/Program.cs
@@ -0,0 +1,14 @@
+﻿int Numbers;
+Console.WriteLine("Введите любое пятизначное число");
+Numbers = Convert.ToInt32(Console.ReadLine());
+int digit1 = Numbers/10000;
+int digit2 = Numbers/1000%10;
+int digit4 = Numbers %100/10;
+int digit5 = Numbers%10;
+{
+    if (digit1 == digit5 && digit2 == digit4)
+    Console.WriteLine("Это число является палиндромом");
+    else
+    Console.WriteLine("Это число не является палиндромом");
+}
+
